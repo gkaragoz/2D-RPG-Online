eb220d205d265dfa9e880a9b64c3f1f6dd600e7e
gkaragoz <gkaragoz96@gmail.com>
2018-12-28 00:16:18 +0300

    Create ObjectDestroy.cs script.

---
+++ Client/2D-RPG-Online/Assets/Scripts/ObjectDestroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour {
+
+    public void Destroy(float time = 0f) {
+        Destroy(this.gameObject, time);
+    }
+
+}
