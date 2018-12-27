3f63a3e38210b4478f470fdc7b4a75659c0a985e
gkaragoz <gkaragoz96@gmail.com>
2018-12-28 01:15:38 +0300

    Spawn character with Portal.cs script.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public Animator animator;
+    public Transform spawnedTransform;
+
+    public void Init(Transform spawnedTransform) {
+        this.spawnedTransform = spawnedTransform;
+    }
+
+    public void Spawn() {
+        spawnedTransform = Instantiate(spawnedTransform);
+        animator.SetTrigger("Move");
+    }
+
+    private void Update() {
+        if (spawnedTransform != null) {
+            spawnedTransform.position = animator.transform.position;
+        }
+    }
+
+}
