104b2a360f9017c76f9fa4b966fe740146b27098
gkaragoz <gkaragoz96@gmail.com>
2018-12-28 18:25:20 +0300

    Create UIManager and Menu scripts.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class Menu : MonoBehaviour {
+
+    public bool isOpen { get; set; }
+
+    public virtual void Show() {
+        gameObject.SetActive(true);
+        isOpen = true;
+    }
+
+    public virtual void Show(GameObject container) {
+        container.SetActive(true);
+        isOpen = true;
+    }
+
+    public virtual void Hide() {
+        gameObject.SetActive(false);
+        isOpen = false;
+    }
+
+    public virtual void Hide(GameObject container) {
+        container.SetActive(false);
+        isOpen = false;
+    }
+
+    public virtual void Toggle() {
+        gameObject.SetActive(!gameObject.activeSelf);
+        isOpen = gameObject.activeSelf;
+    }
+
+    public virtual void Toggle(GameObject container) {
+        container.SetActive(!container.activeSelf);
+        isOpen = container.activeSelf;
+    }
+
+}
---
+++ Client/2D-RPG-Online/Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    #region Singleton
+
+    public static UIManager instance;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(instance);
+    }
+
+    #endregion
+
+    public GameObject loginPanel;
+    public GameObject selectClassPanel;
+
+    public void HideLoginPanel() {
+        loginPanel.SetActive(false);
+    }
+
+    public void HideSelectClassPanel() {
+        selectClassPanel.SetActive(false);
+    }
+
+    public void ShowSelectClassPanel() {
+        selectClassPanel.SetActive(true);
+    }
+
+}
