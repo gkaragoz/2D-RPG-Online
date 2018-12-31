c20448f05427f404e434be21520cdff2061fa420
gkaragoz <gkaragoz96@gmail.com>
2018-12-31 13:34:42 +0300

    Create SessionWatcher.cs script but not working for now.

---
+++ Client/2D-RPG-Online/Assets/Scripts/SessionWatcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionWatcher : MonoBehaviour {
+
+    [Header("Debug")]
+    [SerializeField]
+    [Utils.ReadOnly]
+    private int _sessionIDDebug;
+
+    private static int _sessionID = 0;
+
+    public static int SessionID {
+        get {
+            return _sessionID;
+        }
+    }
+
+    private void Start() {
+        _sessionID++;
+        _sessionIDDebug = _sessionID;
+    }
+
+}
