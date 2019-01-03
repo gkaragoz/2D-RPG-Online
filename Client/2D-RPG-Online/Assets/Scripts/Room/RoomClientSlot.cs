d144514da7b81f8b00cc022a76bbf85ac0edb0b1
gkaragoz <gkaragoz96@gmail.com>
2019-01-04 00:10:09 +0300

    Fill RoomClientSlot.cs script

--- Client/2D-RPG-Online/Assets/Scripts/Room/RoomClientSlot.cs
+++ Client/2D-RPG-Online/Assets/Scripts/Room/RoomClientSlot.cs
@@ -1,9 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomClientSlot : MonoBehaviour {
 
+    [Serializable]
+    public class UISettings {
+        public Image imgFrame;
+        public Image imgCharacter;
+        public Image imgClassIcon;
+        public Toggle isReady;
+        public Image imgLeader;
+        public TextMeshProUGUI txtPlayerName;
+        public TextMeshProUGUI txtCharacterName;
+    }
 
+    [Header("Initialization")]
+    [SerializeField]
+    private UISettings _UISettings;
 
 }
