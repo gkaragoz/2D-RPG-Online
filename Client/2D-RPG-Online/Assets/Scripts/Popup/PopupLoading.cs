3437a56132008b7bf1025a0ac72c65e0fe718d50
gkaragoz <gkaragoz96@gmail.com>
2019-01-07 23:47:20 +0300

    Create PopupManager and PopupLoading scripts.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Popup/PopupLoading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupLoading : Menu {
+
+    [SerializeField]
+    private Image _loadingBar;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    [Utils.ReadOnly]
+    private float _currentAmount;
+
+    private void Update() {
+        if (_currentAmount < 100) {
+            _currentAmount += _speed * Time.deltaTime;
+        }
+
+        _loadingBar.fillAmount = _currentAmount / 100;
+    }
+
+}
---
+++ Client/2D-RPG-Online/Assets/Scripts/Popup/PopupManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupManager : MonoBehaviour {
+
+    #region Singleton
+
+    public static PopupManager instance;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(instance);
+    }
+
+    #endregion
+
+    [SerializeField]
+    private PopupLoading _popupLoading;
+
+    public void ShowLoadingPopup() {
+        _popupLoading.Show();
+    }
+
+    public void HideLoadingPopup() {
+        _popupLoading.Hide();
+    }
+
+}
