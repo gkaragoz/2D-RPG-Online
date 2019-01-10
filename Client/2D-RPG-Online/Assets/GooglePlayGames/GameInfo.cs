22f549455529f6bc02629c1e0ddedc47bc026c58
gkaragoz <gkaragoz96@gmail.com>
2019-01-10 20:06:57 +0300

    Implement Google Play Games join.

--- Client/2D-RPG-Online/Assets/GooglePlayGames/GameInfo.cs
+++ Client/2D-RPG-Online/Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "35777439923"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "35777439923-1mv0akh506k509bnni2kh2auougii2e1.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
---
+++ Client/2D-RPG-Online/Assets/Scripts/Systems/GooglePlay/GooglePlayManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi.Multiplayer;
+using System;
+
+public class GooglePlayManager : MonoBehaviour {
+
+    private void Start() {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            // enables saving game progress.
+            .EnableSavedGames()
+            // registers a callback to handle game invitations received while the game is not running.
+            .WithInvitationDelegate(OnInvitationReceided)
+            // registers a callback for turn based match notifications received while the
+            // game is not running.
+            .WithMatchDelegate(OnMatchNotificationReceived)
+            // requests the email address of the player be available.
+            // Will bring up a prompt for consent.
+            .RequestEmail()
+            // requests a server auth code be generated so it can be passed to an
+            //  associated back end server application and exchanged for an OAuth token.
+            .RequestServerAuthCode(false)
+            // requests an ID token be generated.  This OAuth token can be used to
+            //  identify the player to other services such as Firebase.
+            .RequestIdToken()
+            .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+    }
+
+    public void Login() {
+        // authenticate user:
+        Social.localUser.Authenticate((bool success) => {
+            // handle success or failure
+            Debug.Log("Google Play Login: " + success);
+        });
+    }
+
+    private void Update() {
+        if (Social.localUser.authenticated) {
+            Debug.Log("aq");
+        }
+    }
+
+    private void OnInvitationReceided(Invitation invitation, bool shouldAutoAccept) {
+        throw new NotImplementedException();
+    }
+
+    private void OnMatchNotificationReceived(TurnBasedMatch match, bool shouldAutoLaunch) {
+        throw new NotImplementedException();
+    }
+
+}
