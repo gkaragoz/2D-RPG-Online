75a78f7e45f6c5e8ce7bb35bb8497ce3c973d73c
gkaragoz <gkaragoz96@gmail.com>
2019-01-12 18:13:57 +0300

    Create TutorialManager.cs script.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Systems/Tutorial/TutorialManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TutorialManager : Menu {
+
+    [SerializeField]
+    private GameObject _cameraHolderPrefab;
+    [SerializeField]
+    private GameObject _dummyCamera;
+
+    private void Start() {
+        _cameraHolderPrefab.SetActive(false);
+
+        if (GameObject.Find(_cameraHolderPrefab.name) == null) {
+            Instantiate(_cameraHolderPrefab);
+        }
+    }
+
+}
