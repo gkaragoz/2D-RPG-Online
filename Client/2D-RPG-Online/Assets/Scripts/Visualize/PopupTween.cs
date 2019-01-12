76e5964491fde715ec5b83025a15fef8bc97a382
gkaragoz <gkaragoz96@gmail.com>
2019-01-12 21:56:35 +0300

    Create Popup, Scale, Ticking Tweens.cs scripts and implement it.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Visualize/PopupTween.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PopupTween : MonoBehaviour {
+
+    [Header("Initialization")]
+    [SerializeField]
+    private RectTransform _targetTransform;
+
+    [Header("Settings")]
+    [SerializeField]
+    private float speed = 0.3f;
+    [SerializeField]
+    private float repeatRate;
+    [SerializeField]
+    private float delay;
+    [SerializeField]
+    private Vector2 scaleAmount;
+
+    private void Start() {
+        StartCoroutine(Popup());
+    }
+
+    public IEnumerator Popup() {
+        yield return new WaitForSeconds(delay);
+
+        while (true) {
+            LeanTween.value(_targetTransform.gameObject,
+                _targetTransform.sizeDelta,
+                _targetTransform.sizeDelta + scaleAmount, speed)
+                .setEaseInOutCirc()
+                .setLoopPingPong()
+                .setRepeat(2)
+                .setOnUpdate(
+                (Vector2 val) => {
+                    _targetTransform.sizeDelta = val;
+                }
+            );
+
+            yield return new WaitForSeconds(repeatRate);
+        }
+    }
+
+    private void OnValidate() {
+        if (repeatRate <= 2 * speed) {
+            repeatRate = (2 * speed + 0.1f);
+        }
+    }
+
+}
