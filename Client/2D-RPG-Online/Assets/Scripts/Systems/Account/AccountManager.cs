fa94ae32993f581d8184067972a729c5c2e668d6
gkaragoz <gkaragoz96@gmail.com>
2019-01-13 17:49:23 +0300

    Create AccountManager.cs

---
+++ Client/2D-RPG-Online/Assets/Scripts/Systems/Account/AccountManager.cs
@@ -0,0 +1,42 @@
+﻿using ShiftServer.Proto.Models;
+using UnityEngine;
+
+public class AccountManager : MonoBehaviour {
+    
+    #region Singleton
+
+    public static AccountManager instance;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(instance);
+    }
+
+    #endregion
+
+    public Task initializationProgress;
+
+    [Header("Debug")]
+    [SerializeField]
+    private Account _account;
+
+    public Account Account {
+        get {
+            return _account;
+        }
+        private set {
+            _account = value;
+        }
+    }
+
+    public void Initialize(Account account) {
+        this.Account = account;
+
+        initializationProgress?.Invoke();
+    }
+
+}
