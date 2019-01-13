73ab505c21741e6215fa1809aa18e4a9bbcc9e90
gkaragoz <gkaragoz96@gmail.com>
2019-01-13 17:49:36 +0300

    Create MenuManager.cs

---
+++ Client/2D-RPG-Online/Assets/Scripts/Systems/Menu/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : Menu {
+
+    #region Singleton
+
+    public static MenuManager instance;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(instance);
+    }
+
+    #endregion
+
+    public Task initializationProgress;
+
+    public void Initialize() {
+
+        initializationProgress?.Invoke();
+    }
+
+}
