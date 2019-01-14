d6596b38f86a9b93edeceed607d7e17067c2a929
CAN <gurcaglarogulcan@gmail.com>
2019-01-14 15:17:41 +0300

    Client/Lib: Updated char model with stats and attrb

--- Server/ShiftServer/ShiftServer.Proto/RestModels/CharacterModel.cs
+++ Server/ShiftServer/ShiftServer.Proto/RestModels/CharacterModel.cs
@@ -9,9 +9,31 @@ namespace ShiftServer.Proto.RestModels
         public string account_id;
         public string account_email;
         public string name;
+        public CharacterStats stats;
+        public CharacterAttributes attributes;
         public int class_index;
         public int level;
         public int exp;
+        public int stat_points;
         public DateTime created_at;
     }
+
+    [Serializable]
+    public class CharacterStats
+    {
+
+        public int health;
+        public int mana;
+        public float attack_speed;
+        public float movement_speed;
+    }
+
+    [Serializable]
+    public class CharacterAttributes
+    {
+        public int strength;
+        public int intelligence;
+        public int dexterity;
+    }
+
 }
