9460cc2f59d7c639f61538877f565663f344f650
gkaragoz <gkaragoz96@gmail.com>
2019-01-15 20:02:22 +0300

    Create PlayerHUD.cs

---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/PlayerHUD.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PlayerHUD : Menu {
+
+    [SerializeField]
+    private TextMeshProUGUI _txtName;
+
+    public void SetName(string name) {
+        _txtName.text = name;
+    }
+
+}
