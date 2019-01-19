1e2f4c617a0fd52c977ee4044b3b27a91b81a538
gkaragoz <gkaragoz96@gmail.com>
2019-01-19 20:57:12 +0300

    Delete Hand Painted Texture pack. Implement prototype ground.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    public bool smoothFollow = true;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public float maxHeight = 10f;
+    public float minHeight = 5f;
+    public float heightDampening = 5f;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void LateUpdate() {
+        if (target == null)
+            return;
+
+        if (smoothFollow) {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        } else {
+            Vector3 desiredPosition = target.position + offset;
+            transform.position = desiredPosition;
+        }
+    }
+
+    public void ApplyRotation(int amount) {
+        transform.Rotate(Vector3.right * amount);
+    }
+
+    public void SetTarget(Transform target) {
+        this.target = target;
+    }
+}
