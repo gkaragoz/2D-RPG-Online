d88410789374e89c67de318f8f2129a79bf3a6fe
gkaragoz <gkaragoz96@gmail.com>
2019-01-22 01:09:39 +0300

    Create and Implement WorldSpaceBillboard.cs for player canvas.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/WorldSpaceBillboard.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class WorldSpaceBillboard : MonoBehaviour {
+
+    void LateUpdate() {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+
+}
