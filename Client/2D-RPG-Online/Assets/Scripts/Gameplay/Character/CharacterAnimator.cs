c8f1122a951aafae1d9d9ba73022be959a58159d
gkaragoz <gkaragoz96@gmail.com>
2019-01-24 18:18:01 +0300

    Implement locomotionAnimationSmoothTime variable.

--- Client/2D-RPG-Online/Assets/Scripts/Gameplay/Character/CharacterAnimator.cs
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/Character/CharacterAnimator.cs
@@ -2,18 +2,20 @@
 
 public class CharacterAnimator : MonoBehaviour {
 
+    public float locomotionAnimationSmoothTime = 0.1f;
+
     private Animator _animator;
 
     private void Start() {
-        _animator = GetComponentInChildren< Animator>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     public void OnMove(Vector3 direction) {
-        _animator.SetFloat("InputMagnitude", direction.magnitude);
+        _animator.SetFloat("InputMagnitude", direction.magnitude, locomotionAnimationSmoothTime, Time.deltaTime);
     }
 
     public void OnStop() {
-        _animator.SetFloat("InputMagnitude", 0);
+        _animator.SetFloat("InputMagnitude", 0, locomotionAnimationSmoothTime, Time.deltaTime);
     }
     
     public void OnAttack() {
