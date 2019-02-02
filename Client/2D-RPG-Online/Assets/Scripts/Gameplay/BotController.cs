937682687e293b7916a986008f9731f8c5fc0cd1
gkaragoz <gkaragoz96@gmail.com>
2019-02-02 10:11:08 +0300

    Create BotController.cs script

---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/BotController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : LivingEntity {
+
+    public override void Awake() {
+        base.Awake();
+    }
+
+}
