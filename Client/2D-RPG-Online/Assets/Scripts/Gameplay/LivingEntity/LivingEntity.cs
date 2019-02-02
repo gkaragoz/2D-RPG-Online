eca5cf22ddde75c63f019fc13c7e48fc62a32847
gkaragoz <gkaragoz96@gmail.com>
2019-02-02 10:10:48 +0300

    Create Living Entity scriptable object and script itself.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/LivingEntity/LivingEntity.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, ILivingEntity {
+
+    [Header("Initialization")]
+    [SerializeField]
+    private LivingEntity_SO _livingEntityDefinition_Template;
+
+    [Header("Debug")]
+    [SerializeField]
+    [Utils.ReadOnly]
+    private LivingEntity_SO _livingEntityDefinition;
+
+    public enum Type {
+        Player,
+        Friend,
+        Enemy,
+        Object
+    }
+
+    public bool IsDeath { get { return _livingEntityDefinition.isDeath; } set { _livingEntityDefinition.isDeath = value; } }
+
+    public Type EntityType { get { return _type; } }
+
+    [SerializeField]
+    private Type _type;
+
+    public virtual void Awake() {
+        if (_livingEntityDefinition_Template != null) {
+            _livingEntityDefinition = Instantiate(_livingEntityDefinition_Template);
+        }
+    }
+
+    public virtual void TakeDamage(int damage) {
+
+    }
+
+    public virtual void OnDeath() {
+
+    }
+
+    public virtual void Attack() {
+
+    }
+
+    public virtual void MoveByInput() {
+
+    }
+
+    public virtual void MoveToPosition(Vector3 position) {
+
+    }
+
+    public virtual void Stop() {
+
+    }
+
+    public virtual void Rotate() {
+
+    }
+
+    public virtual void Destroy() {
+
+    }
+
+}
---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/LivingEntity/LivingEntity_SO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Living Entity", menuName = "Scriptable Objects/Living Entity")]
+public class LivingEntity_SO : ScriptableObject {
+
+    public bool isDeath;
+
+}
