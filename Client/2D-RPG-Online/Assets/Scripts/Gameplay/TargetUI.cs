09bc29acda891ce2a05dc9fa40dfd5549dc5e4bf
gkaragoz <gkaragoz96@gmail.com>
2019-02-05 14:38:04 +0300

    Create TargetUI script.

---
+++ Client/2D-RPG-Online/Assets/Scripts/Gameplay/TargetUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetUI : Menu {
+
+    [SerializeField]
+    private Image _imgTargetHealthBar;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI _txtTargetName;
+
+    private CharacterStats _characterStats;
+
+    public void UpdateUI() {
+        if (_characterStats != null) {
+            SetName();
+            SetHealthBar();
+        }
+    }
+
+    public void OpenUI(CharacterStats characterStats) {
+        this._characterStats = characterStats;
+
+        UpdateUI();
+        Show();
+    }
+
+    public void HideUI() {
+        Hide();
+    }
+
+    private void SetName() {
+        _txtTargetName.text = _characterStats.GetName();
+    }
+
+    private void SetHealthBar() {
+        _imgTargetHealthBar.fillAmount = ((float)_characterStats.GetCurrentHealth() / (float)_characterStats.GetMaxHealth());
+    }
+
+}
