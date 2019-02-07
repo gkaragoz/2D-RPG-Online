678e59808016c976f89f867c6389451201c7f9de
ManaShift <gurcaglarogulcan@gmail.com>
2019-02-07 21:01:27 +0300

    Add Priest,Archer Basic Attack. Add Whole VFX Packages

---
+++ Client/2D-RPG-Online/Assets/FX/FX_Simple_Shining_Pack1/Script/DemoController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoController : MonoBehaviour {
+
+	// effect GameObject
+	public GameObject eff1;
+	public GameObject eff2;
+	public GameObject eff3;
+	public GameObject eff4;
+	public GameObject eff5;
+
+	public GameObject eff_name;	// effect_name_label
+
+	public int count = 0;
+
+
+	void Start () {
+		eff1.SetActive (true);
+		eff2.SetActive (false);
+		eff3.SetActive (false);
+		eff4.SetActive (false);
+		eff5.SetActive (false);
+		count = 0;
+		NameChange ();
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			count++;
+
+			switch (count) 
+			{
+			case 0: 
+				eff1.SetActive (true);
+				eff2.SetActive (false);
+				eff3.SetActive (false);
+				eff4.SetActive (false);
+				eff5.SetActive (false);
+				break;
+			case 1:
+				eff1.SetActive (false);
+				eff2.SetActive (true);
+				eff3.SetActive (false);
+				eff4.SetActive (false);
+				eff5.SetActive (false);
+				break;
+			case 2:
+				eff1.SetActive (false);
+				eff2.SetActive (false);
+				eff3.SetActive (true);
+				eff4.SetActive (false);
+				eff5.SetActive (false);
+				break;
+			case 3:
+				eff1.SetActive (false);
+				eff2.SetActive (false);
+				eff3.SetActive (false);
+				eff4.SetActive (true);
+				eff5.SetActive (false);
+				break;
+			case 4:
+				eff1.SetActive (false);
+				eff2.SetActive (false);
+				eff3.SetActive (false);
+				eff4.SetActive (false);
+				eff5.SetActive (true);
+				break;
+			case 5: 
+				eff1.SetActive (true);
+				eff2.SetActive (false);
+				eff3.SetActive (false);
+				eff4.SetActive (false);
+				eff5.SetActive (false);
+				break;
+
+			}
+			if (count == 5) {
+				count = 0;
+			}
+			NameChange ();
+		}
+	}
+
+
+
+	void NameChange(){
+		if (eff1.activeSelf == true) {
+			eff_name.GetComponent<Text> ().text = eff1.name;
+		}
+		if (eff2.activeSelf == true) {
+			eff_name.GetComponent<Text> ().text = eff2.name;
+		}
+		if (eff3.activeSelf == true) {
+			eff_name.GetComponent<Text> ().text = eff3.name;
+		}
+		if (eff4.activeSelf == true) {
+			eff_name.GetComponent<Text> ().text = eff4.name;
+		}
+		if (eff5.activeSelf == true) {
+			eff_name.GetComponent<Text> ().text = eff5.name;
+		}
+
+	}
+}
---
+++ Client/2D-RPG-Online/Assets/FX/ParticleDistortTexture/ReadMe.txt
@@ -0,0 +1,45 @@
+***********************************
+ParticleDistortTexture
+Version 1.1
+Date 15 Feb 2017
+For Unity 5.5.1 or up
+
+by Moonflower Carnivore
+***********************************
+
+Particle Distort Texture bundles 2 sets of assets, the distort 
+texture shaders for particle system and particle effect library with 
+customized textures. The particles make use of the water distortion 
+calculations to create special visual effects which is all done 
+by blending the textures.
+
+The distortion works in Unity Personal edition because it only 
+distorts the base texture mounted on the material rather than 
+the graphic content behind the texture. 
+In any case, our distort shader allows you to achieve the animated 
+distortion effect without using animated texture because the 
+distortion is being calculated in real time. This has the 
+advantage of adjustability in terms of distortion shape, size and speed.
+
+Please check the webplayer demo:
+http://www.dexint.net/unity/20150406_ParticleDistortTexture/
+
+
+For detailed instructions about using this Simple SeeThrough Shader please 
+read our online google doc here:
+https://docs.google.com/document/d/1FiE2b77unmiTwOHUDvMnQ7j3n3Kwy8kHbsGwgXK3sN4/edit?usp=sharing
+
+
+We are Moonflower Carnivore and we provide game assets like particle 
+effects and simple shaders. Please like our Facebook page 
+https://www.facebook.com/MoonflowerCarnivore
+and rate our packages in Unity Asset Store if you like our work! 
+
+
+
+**************UPDATE LOG************
+[15 Feb 2017] Version 1.1
+Upgrade to Unity5.5.1, updated and optimized shader, demo scenes and effects
+
+[12 Jul 2015] Version 1.0
+First Submission
